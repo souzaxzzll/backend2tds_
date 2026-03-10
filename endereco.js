b26fa7455cf718642ecb85e5ce5d8f56ec94ac18
felpsgames-cityboy <felipe.figueiroa@escola.pr.gov.br>
2026-03-10 11:40:25 -0300

    Add files via upload

---
+++ endereco.js
@@ -0,0 +1,8 @@
+console.log("Endereço Completo");
+let rua = "Rua das Flores";
+let numero = 77;
+let cidade = "Curitiba";
+
+let endereco = rua + ", " + numero + " - " + cidade;
+//console.log (endereco);
+console.log("O Endereço correto é: " + endereco);                                                                                                                                                     1 
