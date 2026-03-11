dbe3578abd560f423afcb2cf8aa958bd3a1d87df
felpsgames-cityboy <felipe.figueiroa@escola.pr.gov.br>
2026-03-11 08:48:40 -0300

    Add files via upload

---
+++ relatorio.js
@@ -0,0 +1,9 @@
+//Relatório de notas
+let aluno = "Felipe";
+let nota1 = 8.5;
+let nota2 = 5.0;
+
+let media = (nota1 + nota2) / 2;
+let relatorio = "Aluno: " + aluno + "| Notas: " + nota1 + " e " + nota2 + "| Média de notas " + media;
+console.log("Relatório de Notas");
+console.log(relatorio);
