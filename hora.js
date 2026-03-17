e1f058b68c17387a7559e330aea1807a9d9a7b04
felpsgames-cityboy <felipe.figueiroa@escola.pr.gov.br>
2026-03-17 14:39:48 +0000

    versao2

--- hora.js
+++ hora.js
@@ -1,3 +1,10 @@
 //Valores inicias do dia
 let valorPorHora = 50;
 let horasTrabalhadas = 8;
+
+console.log("---Resumo do Expediente---");
+console.log(`Horas Iniciais: ${horasTrabalhadas}h. `);
+
+//git add hora.js
+//git commit -m "versao2"
+//git push
