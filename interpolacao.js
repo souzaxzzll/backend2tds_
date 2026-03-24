31b4399f0022290d75c5d9b34544d13d4aab255b
felpsgames-cityboy <felipe.figueiroa@escola.pr.gov.br>
2026-03-24 11:40:34 -0300

    Add files via upload

---
+++ interpolacao.js
@@ -0,0 +1,10 @@
+const nome = "Felipe";
+const idade = 16;
+const anoAtual = 2026;
+
+const anoNascimento = anoAtual - idade;
+const idadeEmDezAnos = idade + 10;
+
+console.log("O Ano de nascimento é: " + anoNascimento);
+console.log(`${nome} possui ${idade} anos.`);
+console.log(`Daqui há 10 anos, terá ${idadeEmDezAnos} anos.`);
