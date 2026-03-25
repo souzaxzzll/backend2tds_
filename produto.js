331f76b29688e62396281f1a17484822541b13e1
felpsgames-cityboy <felipe.figueiroa@escola.pr.gov.br>
2026-03-25 11:54:47 +0000

    versao1

---
+++ produto.js
@@ -0,0 +1,8 @@
+const produto = "Mochila Impermeável";
+let preco = 189.90;
+let estoque = 14;
+const categoria = "acessórios"
+
+console.log(`A ${produto} custa R$${preco.toFixed(2)}.`);
+console.log(`Temos ${estoque} itens no estoque.`);
+console.log(`O produto pertence a categoria ${categoria}.`);
