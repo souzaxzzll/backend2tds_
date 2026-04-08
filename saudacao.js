b2e50057c1ef139184d4b1a6b922b7634963647d
felpsgames-cityboy <felipe.figueiroa@escola.pr.gov.br>
2026-04-08 14:20:14 +0000

    v2

--- saudacao.js
+++ saudacao.js
@@ -18,3 +18,13 @@ function soma(numero1, numero2) {
 };
 
 console.log(soma(5,3));
+
+function verificarPar(numero) {
+    if(numero %  2 === 0) {
+        return "O número é par"
+    } else {
+        return "O número é impar"
+    }
+};        
+
+console.log(verificarPar(3));
