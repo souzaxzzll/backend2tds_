4e9a037da528e163d71d00fb87fa0f5fbfef69e8
felpsgames-cityboy <felipe.figueiroa@escola.pr.gov.br>
2026-04-15 13:44:12 +0000

    v1

---
+++ lista.js
@@ -0,0 +1,11 @@
+const alunos = ["Alice", "Bruna", "Carla"];
+
+alunos.push("Diego");
+alunos.unshift("Lopes");
+console.log(alunos);
+
+console.log(alunos.indexOf("Diego"));
+console.log(alunos.indexOf("Paula"));
+
+console.log(alunos.includes("Paula"));
+console.log(alunos.includes("Carla"));
