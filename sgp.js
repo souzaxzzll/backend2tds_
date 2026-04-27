75a710982b0bcb36cf22079d2cc8f826810a1c28
felpsgames-cityboy <felipe.figueiroa@escola.pr.gov.br>
2026-04-27 13:51:37 +0000

    v1

---
+++ sgp.js
@@ -0,0 +1,22 @@
+//SISTEMA DE GERENCIAMENTO DE PERSONAGEM
+
+function calcularPontuacaoTotal(historicoDePontos) {
+    const somarRodada = function(acumulador, pontosDaFase){
+        const novaSoma = acumulador + pontosDaFase;
+        return novaSoma;
+    };
+    
+    const pontuacaoFinal = historicoDePontos.reduce(somarRodada, 0);
+    return pontuacaoFinal;
+};
+
+function filtrarMissoesConcluidas(listaDeMissoes) {
+    const verificarStatusDaMissao = function(missao) {
+        const foiFinalizada = missao.status === "concluida"; //=== Igualdade ESTRITA
+        return foiFinalizada;
+    }
+
+    const apenasConcluidas = listaDeMissoes.filter(verificarStatusDaMissao);
+    return apenasConcluidas;
+};
+
