ec86c7fb4855798a12604a1bb052f83265c0336f
felpsgames-cityboy <felipe.figueiroa@escola.pr.gov.br>
2026-04-29 13:52:04 +0000

    V2

--- sgp.js
+++ sgp.js
@@ -14,9 +14,22 @@ function filtrarMissoesConcluidas(listaDeMissoes) {
     const verificarStatusDaMissao = function(missao) {
         const foiFinalizada = missao.status === "concluida"; //=== Igualdade ESTRITA
         return foiFinalizada;
-    }
+    };
 
     const apenasConcluidas = listaDeMissoes.filter(verificarStatusDaMissao);
     return apenasConcluidas;
 };
+
+function atualizarInventario(inventarioAtual, acao, nomeDoItem) {
+    let novoInventario;
+
+    if(acao ==="pegar") {
+        const inventarioComItemNovo = [inventarioAtual, nomeDoItem];
+        novoInventario = 
+    } else if (acao === "descartar")
+}
+
+const pontosDoJogador = [100, 50, 200 ,10]
+console.log(pontosDoJogador);
+
 
