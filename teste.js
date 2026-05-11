0c8e0a78c82a04c359e84ab3380c43c8c46ee5f0
felpsgames-cityboy <felipe.figueiroa@escola.pr.gov.br>
2026-05-11 13:54:57 +0000

    v1

---
+++ teste.js
@@ -0,0 +1,26 @@
+//1. Declare 5 variáveis para representar uma viagem (destino, nome do viajante, cidade de origem, duração em dias e meio de transporte)
+//2. Organizar uma forma de imprimir esses valores com interpolação ou saída individual
+//Nota: Ao fazer interpolação, inserir crase
+
+const destino = "Foz do Iguaçu";
+const nomeViajante = "Felipe";
+const cidadeOrigem = "Curitiba";
+let duracao = 8;
+let transporte = "Carro";
+
+console.log(`O viajante ${nomeViajante} deseja viajar de ${cidadeOrigem} até ${destino}, de ${transporte} em um percurso de ${duracao} horas. `);
+
+//3. Crie duas variáveis númericas com a custo da passagem, hospedagem e alimentacao
+//4. Exiba uma soma dos valores
+
+let hospedagem = 600;
+let alimentacao = 300;
+
+let total = hospedagem + alimentacao;
+
+console.log("0 total dos gastos será: R$ " + total + "reais.")
+
+
+
+
+
