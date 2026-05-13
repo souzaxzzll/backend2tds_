6d0d781bfe3a4d9baf008bdee0e12040955460c8
felpsgames-cityboy <felipe.figueiroa@escola.pr.gov.br>
2026-05-13 13:53:15 +0000

    v1

---
+++ desafio.js
@@ -0,0 +1,9 @@
+//1. Declarar 5 variáveis para representar um funcionário
+//Fazer uma saída de forma interpolada (`${}`)
+
+let nomeFuncionario = "Felipe";
+let idade = "18";
+let cargo = "Administrador";
+let salario = "R$ 2.450";
+
+console.log(); 
