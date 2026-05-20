4b985679fe81e9d05ac4f47f3f6aa2269470e49a
felpsgames-cityboy <felipe.figueiroa@escola.pr.gov.br>
2026-05-20 13:55:22 +0000

    v3

--- desafio.js
+++ desafio.js
@@ -1,19 +1,23 @@
 //1. Declarar 5 variáveis para representar um funcionário
 //Fazer uma saída de forma interpolada (`${}`)
 
-const nomeFuncionario = "Felipe S.";
+const nomeFuncionario = "Felipe Souza";
 let idade = "18";
-let cargo = "Administrador";
-let salario = "R$ 2.758";
+let anosEmpresa = 4;
+let cargo = "Administrador Pleno";
+let salario = "R$ 3.758";
 let salarioBonus = "R$ 459.50";
-let descontos = "R$ 223,90";
+let desconto = 10
+let valorTotal = "R$ 3.795,75";
 let estaAtivo = true;
 
 numero1 = 5;
 numero2 = 10;
 
-console.log("--Sobre o Funcionário--"); 
-console.log(`Nome: ${nomeFuncionario} | Cargo: ${cargo}`);
+console.log("       --Sobre o Funcionário--"); 
+console.log(`Nome: ${nomeFuncionario}     | Cargo: ${cargo}`);
+console.log(`Ìdade: ${idade}              | Anos de Empresa: ${anosEmpresa}`);
 console.log(`Salário Base: ${salario} | Bônus: ${salarioBonus} `);
-console.log(`Desconto: ${descontos}`);
-console.log(`Ìdade: ${idade}`);
+console.log(`Desconto: ${desconto}%`)
+console.log(`Valor líquido total: ${valorTotal}`);
+ 
